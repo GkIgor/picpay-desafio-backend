56055d0c8270a964c4eb4732264b106f96e1c461
GkIgor <igorf0660@gmail.com>
2024-06-19 23:48:04 -0300

    feat: adicionado dotenv para ler variaveis de ambiente

--- bin/picpay_desafio_backend.dart
+++ bin/picpay_desafio_backend.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
-
+import 'package:dotenv/dotenv.dart';
 import 'core/config/app.config.dart';
 import 'core/config/app.obs.dart';
 
 void main(List<String> args) async {
+  DotEnv().load();
+
   await Future.wait([bootstrapApp(), startObs()]);
 }
