725ee4310d48f64cc1a70f9465d013937745084d
GkIgor <igorf0660@gmail.com>
2024-06-19 23:49:23 -0300

    refactor: porta alterada para seguir as variaveis de ambiente

--- bin/core/config/app.config.dart
+++ bin/core/config/app.config.dart
@@ -1,19 +1,15 @@
 import 'dart:io';
-
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
-
+import '../../environments/environments.dart';
 import 'app.router.dart';
 
 Future<void> bootstrapApp() async {
   final ip = InternetAddress.anyIPv4;
-
   final pipeline = Pipeline().addMiddleware(logRequests());
-
   final handler = pipeline.addHandler(router.call);
-
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final port = Environments.PORT;
   final server = await serve(handler, ip, port);
 
-  print('Server listening on port ${server.port}');
+  print('Server listening on port $ip:${server.port}');
 }
