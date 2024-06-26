3700f039ee4daa30241187906d90dd3864907181
GkIgor <igorf0660@gmail.com>
2024-06-26 16:49:55 -0300

    feat: rota para usuarios criada

--- bin/core/config/app.router.dart
+++ bin/core/config/app.router.dart
@@ -1,9 +1,12 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
+import '../../controller/users/users.controller.dart';
+
 final router = Router()
   ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
+  ..get('/echo/<message>', _echoHandler)
+  ..post('/users', createUser);
 
 Response _rootHandler(Request req) {
   return Response.ok('Hello, World!\n');
