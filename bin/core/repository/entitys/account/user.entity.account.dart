1959798ea28e238e0328e4f6abce88acd4c6a517
GkIgor <igorf0660@gmail.com>
2024-06-27 15:12:15 -0300

    feat: pequenas melhorias e correçoes

--- bin/core/repository/entitys/account/user.entity.account.dart
+++ bin/core/repository/entitys/account/user.entity.account.dart
@@ -1,26 +1,43 @@
 import '../../../class/account.class.dart';
+import '../user/users.entity.dart';
 
 class UserAccount extends Account {
   int userWallet;
   UserAccount({
     required super.agency,
-    required super.account,
-    required super.verifier,
-    required super.createdAt,
-    required super.id,
+    super.account,
+    super.verifier,
+    super.createdAt,
+    required super.accountId,
     required this.userWallet,
+    super.balance,
+    required super.lastUpdated,
   });
 
   factory UserAccount.fromMap(Map<String, dynamic> map) {
     return UserAccount(
-      id: map['id'],
+      accountId: map['id'],
       agency: map['agency'],
       account: map['account'],
       verifier: map['verifier'],
       createdAt: DateTime.parse(map['created_at']),
       userWallet: map['user_wallet'],
+      balance: map['balance'],
+      lastUpdated: DateTime.now(),
     );
   }
+
+  factory UserAccount.byUser(User user, String agency) {
+    return UserAccount(
+      accountId: user.id!,
+      createdAt: DateTime.now(),
+      userWallet: user.createWallet().walletId!,
+      balance: 0,
+      lastUpdated: DateTime.now(),
+      agency: agency,
+    );
+  }
+
   @override
   String toString() {
     return '$agency, $account, $verifier, $userWallet';
